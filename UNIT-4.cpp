7df42e9db5aeab5eb50db05b54d9bf9dbea4239b
Aditya Vr <adityaverma4768@gmail.com>
2023-10-03 10:59:22 +0530

    C++

--- UNIT-4.cpp
+++ UNIT-4.cpp
@@ -190,3 +190,64 @@ int main(){
 	N1--;
 	
 }*/
+
+
+
+
+//BINARY OPERATORS
+/*#include<iostream>
+using namespace std;
+class demo{
+	int a;
+	int b;
+	public:
+		void get(int a1, int b1){
+			a=a1;
+			b=b1;
+		}
+		void operator +(demo &d2){
+			cout<<a+d2.a<<" "<<b+d2.b;
+		}
+};
+int main(){
+	demo d,d1;
+	d.get(10,40);
+	d1.get(20,50);
+	d.operator +(d1);
+	return 0;
+}*/
+
+
+/*#include<iostream>
+using namespace std;
+class demo{
+	int a;
+	int b;
+	public:
+		void get(int a1, int b1){
+			a=a1;
+			b=b1;
+		}
+		demo operator +(demo &d2){
+			demo s;
+			s.a = a+d2.a;
+			s.b = b+d2.b;
+			return s;
+		}
+		void show(){
+			cout<<a<<" "<<b;
+		}
+};
+int main(){
+	demo d,d1,sum;
+	d.get(10,40);
+	d1.get(20,50);
+	sum = d+d1;
+	//d.operator +(d1);
+	sum.show();
+	return 0;
+}*/
+
+
+
+
