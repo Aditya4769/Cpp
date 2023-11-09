ed9488dfb4b4fea4cc6cd3414ccf62796ed883a6
Aditya Vr <adityaverma4768@gmail.com>
2023-11-09 08:43:39 +0530

    C++

--- UNIT-6.cpp
+++ UNIT-6.cpp
@@ -454,7 +454,7 @@ int main(){
 
 
 
-#include<iostream>
+/*#include<iostream>
 using namespace std;
 template<class T1,class T2>
 class Test{
@@ -474,4 +474,354 @@ int main(){
 	test1.show();
 	Test<int,char>test2(10,'c');
 	test2.show();
+}*/
+
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+template<class T>
+class mypair{
+	T a,b;
+	public:
+		mypair(T first,T second){
+			a=first;
+			b=second;
+		}
+		T getmax();
+};
+template<class T>
+T mypair<T>::getmax(){
+	T rival;
+	rival = a>b?a:b;
+	return rival;
 }
+int main(){
+	mypair<int>obj(10,20);
+	cout<<"Max value is: "<<obj.getmax();
+	mypair<float>obj1(10.2,10.5);
+	cout<<"\nMax value is: "<<obj1.getmax();
+}*/
+
+
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+template<class T>
+class sample{
+private:
+T value1,value2;
+public:
+void get();
+void sum();	
+};
+template<class T>
+void sample<T>::get(){
+	cin>>value1>>value2;
+}
+template<class T>
+void sample<T>::sum(){
+	cout<<"Sum of Both values are: "<<value1+value2<<endl;
+}
+int main(){
+	sample<int>obj;
+	obj.get();
+	obj.sum();
+}*/
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+template<class T>
+class sample{
+	private:
+		T a;
+	public:
+		void sum(int n);
+};
+template<class T>
+void sample<T>::sum(int n){
+	int i;
+	T s=0;
+	for(i=0;i<n;i++){
+		cin>>a;
+		s=s+a;
+	}
+	cout<<s;
+}
+
+int main(){
+	sample<int>s1;
+	s1.sum(5);
+}*/
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+template<class T1,class T2>
+class sample{
+	private:
+		T1 x;
+		T2 y;
+	public:
+		sample(T1 a,T2 b);
+		void putdata();
+};
+template<class T1,class T2>
+sample<T1,T2>::sample(T1 a,T2 b){
+	x=a;
+	y=b;
+}
+template<class T1,class T2>
+void sample<T1,T2>::putdata(){
+	cout<<x<<"\t"<<y<<endl;
+}
+int main(){
+	sample<int,int>s1(10,20);
+	sample<float,float>s2(10,2.30);
+	sample<char,int>s3('x',20);
+	s1.putdata();
+	s2.putdata();
+	s3.putdata();
+}*/
+
+
+
+
+
+
+
+/*#include<iostream>
+using namespace std;
+template<class T>
+class alpha{
+	protected:
+		T a;
+	public:
+		void check(){
+			cout<<"\nHello";
+		}
+};
+template<class T,class T1>
+class beta:public alpha<T>{
+	private:
+		T1 b;
+		public:
+			void get(){
+				cin>>alpha<T>::a>>b;
+			}
+			void show(){
+				cout<<"a: "<<alpha<T>::a<<endl;
+				cout<<"b: "<<b<<endl;
+				alpha<T>::check();
+			}
+			
+};
+int main(){
+	beta<int,float>b1;
+	beta<float,int>b2;
+	cout<<"\nEnter a(int) and b(float): ";
+	b1.get();
+	b1.show();
+	cout<<"\nEnter a(float) and b(int): ";
+	b2.get();
+	b2.show();
+}*/
+
+
+
+
+
+
+//__________________________Standard Template Library_______________________________
+
+/*#include<iostream>
+using namespace std;
+#include<vector>
+int main(){
+	vector<int> v1(5);
+	cout<<v1.size()<<endl;
+	for(int i=0;i<v1.size();i++){
+		v1[i]=i;
+	}
+	for(int i=0;i<v1.size();i++){
+		cout<<v1[i]<<endl;
+	}
+	for(int i=5;i<10;i++){
+		v1.push_back(i);
+	}
+	cout<<"Size is: "<<v1.size()<<"\n";
+	for(int i=0;i<v1.size();i++){
+		cout<<v1[i]<<"\t";
+	}
+	v1.pop_back();
+	v1.pop_back();
+	cout<<"\nSize is: "<<v1.size()<<"\n";
+	for(int i=0;i<v1.size();i++){
+		cout<<v1[i]<<"\t";
+	}
+	vector<int>::iterator v;
+	v=v1.begin();
+	while(v!=v1.end()){
+		cout<<"\nValue of v: "<<*v<<endl;
+		v++;
+		
+	}
+	return 0;
+	
+	
+}*/
+
+
+
+
+
+
+/*#include<iostream>
+#include<vector>
+using namespace std;
+main(){
+	vector<int> v1;
+	cout<<v1.size()<<endl;
+	v1.push_back(5);
+	v1.push_back(6);
+	v1.push_back(7);
+	v1.push_back(2);
+	v1.push_back(1);
+	cout<<"Size of Vector is: "<<v1.size()<<endl;
+	cout<<"Element in arrays are: "<<endl;
+	int i;
+	for(i=0;i<v1.size();i++){
+		cout<<v1[i]<<"\t";
+	}
+	cout<<"\nUsing iterator for displaying: "<<endl;
+	vector<int>::iterator it;
+	for(it = v1.begin();it!=v1.end();it++){
+		cout<<*it<<"\t";
+	}	
+	cout<<endl;
+	v1.push_back(9);
+	v1.push_back(8);
+	cout<<endl;
+	for(it=v1.begin();it!=v1.end();it++){
+		cout<<*it<<"\t";
+	}
+	cout<<endl;
+	it=v1.begin();
+	it++;
+	it++;
+	v1.insert(it,15);
+	cout<<"After Insertion: "<<endl;
+	for(it=v1.begin();it!=v1.end();it++){
+		cout<<*it<<"\t";
+	}
+	cout<<endl;
+	v1.erase(v1.begin()+4);
+	cout<<"After Deletion: "<<endl;
+	for(it=v1.begin();it!=v1.end();it++){
+		cout<<*it<<"\t";
+	}
+	cout<<endl;
+	cout<<"Size Before is: "<<v1.size()<<endl;
+	v1.clear();
+	cout<<"Size After clear is: "<<v1.size();
+}*/
+
+
+
+
+
+
+/*#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+	vector<int> v1(10);
+	cout<<"Size is: "<<v1.size()<<endl;
+	for(int i=0;i<v1.size();i++){
+		v1[i]=i;
+	}
+	vector<int>::iterator i=v1.begin();
+	i=i+3;
+	v1.insert(i,100);
+	v1.erase(v1.begin()+1,v1.begin()+5);
+	for(int i=0;i<v1.size();i++){
+		cout<<v1[i]<<"\t";
+	}
+	cout<<"\nSize is: "<<v1.size()<<endl;
+	v1.clear();
+	cout<<"Size After Clear is: "<<v1.size();
+}*/
+
+
+
+
+
+
+
+
+
+/*#include<iostream>
+#include<list>
+using namespace std;
+void display(list<int> &l){
+	list<int>::iterator p;
+	for(p=l.begin();p!=l.end();p++){
+		cout<<*p<<"\t";
+	}
+}
+int main(){
+	list<int> list1;
+	list<int> list2(5);
+	for(int i=0;i<3;i++){
+		list1.push_back(i);
+		cout<<"\nDisplaying First List: ";
+		display(list1);
+	}
+		list<int>::iterator p;
+		for(p=list2.begin();p!=list2.end();p++){
+			*p=1;
+			cout<<"\nDisplaying Second list: ";
+			display(list2);
+			cout<<"\nPushing element at front: ";
+			list1.push_front(100);
+			display(list1);
+			cout<<"\nPoping element from front: ";
+			list2.pop_front();
+			display(list2);
+			cout<<"\nSorting First list: ";
+			list1.sort();
+			display(list2);
+			cout<<"\nMerging list: ";
+			list1.merge(list2);
+			display(list1);
+			cout<<endl;
+			list1.reverse();
+			display(list1);
+		}
+
+}*/
+
+
+
+
+#include<iostream>
+using namespace std;
+#include<list>
+
